7333b33c805a2de9fa8a2fbc116aff66c7130531
JeonMinGeon <wjsxorjs@ajou.ac.kr>
2021-04-22 18:45:57 +0900

    JS파일 컨벤션 맞춤 - js파일 이름 camelCase로 변환

---
+++ components/GroupInfo.js
@@ -0,0 +1,55 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-labels */
+/* eslint-disable no-alert */
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+*/
+
+import React from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+
+
+
+export default function UserInfo(props) {
+    const star = require('../assets/icons/star.png');
+    const time = require('../assets/icons/pin.png');
+    const user = require('../assets/icons/user.png');
+    let logo;
+    switch (props.logo){
+        case 'minus': logo = require('../assets/icons/pizza.png'); break;
+        case 'home': logo = require('../assets/icons/fries.png'); break;
+        case 'gps': logo = require('../assets/icons/noodle.png'); break;
+
+    }
+        return (
+            <>
+                <TouchableOpacity
+                    onPress={()=>alert(`${props.shopName}`)}
+                >
+                    <View style={props.styleGroupInfo}>
+                        <Image style={props.styleLogoImage} source={logo}/>
+                        <View>
+                            <Text style={props.styleShopText}>{props.shopName}</Text>
+                            <View style={props.styleRating}>
+                                <Image style={props.styleStarImage} source={star}/>
+                                <Text style={props.styleRateText}>{props.rate}/5.0</Text>
+                            </View>
+                            <View style={props.styleDeliveryTime}>
+                                <Image style={props.styleTimeImage} source={time}/>
+                                <Text style={props.styleDeliveryTimeText}> {props.time}</Text>
+                            </View>
+                        </View>
+                        <View style={props.styleGroupNumber}>
+                            <Image style={props.styleUserImage} source={user} />
+                            <Text style={props.groupNumberText}>{props.current}/{props.max}</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </>
+        );
+}
